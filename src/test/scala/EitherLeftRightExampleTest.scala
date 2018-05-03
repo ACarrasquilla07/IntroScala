ae6ff6b21eebfe57f1b8c156c62b032db6cdfc08
Andres Carrasquilla Git <andresjulian.carrasquilla@gmail.com>
2018-05-02 23:08:23 -0500

    Test Left

--- src/test/scala/EitherLeftRightExampleTest.scala
+++ src/test/scala/EitherLeftRightExampleTest.scala
@@ -2,10 +2,17 @@ import org.scalatest.{AsyncWordSpec, Matchers}
 
 class EitherLeftRightExampleTest extends AsyncWordSpec with Matchers{
 
-  "EitherTest" should{
+  "EitherRightTest" should{
     "" in{
       val valor = EitherLeftRightExample.divide(9,9)
       Right(1) should be(valor)
+    }
+  }
+
+  "EitherLeftTest" should{
+    "" in{
+      val valor = EitherLeftRightExample.divide(9,0)
+      Left("No es posible dividir por cero") should be(valor)
     }
   }
 }
